d47571496d40b95bccd571823f64e214bf894b8e
Mehmet Can Kılıç <Mehmet Can Kl@MCK-XPS>
2026-07-10 13:22:06 +0300

    .

--- BBGFinance/Default.aspx.cs
+++ BBGFinance/Default.aspx.cs
@@ -17,7 +17,7 @@ namespace BBGFinance
         { 
             if (!IsPostBack)
             {
-                lblAdSoyad.Text = SessionManager.AdSoyad;
+                lblAdSoyad.Text = SessionManager.AdSoyad; 
                 lblTarih.Text   = DateTime.Now.ToString("dd MMMM yyyy, dddd",
                                     new System.Globalization.CultureInfo("tr-TR"));
 
